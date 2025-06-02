c054f299a858d04bbaa5bc44d1bdf89ca0dd46ed
Arun pandian <arunpandian972000@gmail.com>
2025-06-02 18:03:01 +0530

    deploy v-61

--- components/Navbuttons/LoginNavButton.tsx
+++ components/Navbuttons/LoginNavButton.tsx
@@ -4,21 +4,17 @@ import React, { useState, useEffect } from "react";
 import Cookies from "js-cookie";
 import ProfileButton from "./ProfileButton";
 import { CiLogin } from "react-icons/ci";
+import Link from "next/link";
 import AuthModal from "@/app/test/components/LoginFormTest";
 
 const LoginNavButton = () => {
-  const [token, setToken] = useState<string | null>(null);
+  const [token, setToken] = useState(null);
   const [showAuth, setShowAuth] = useState(false);
-  const [mounted, setMounted] = useState(false);
 
   useEffect(() => {
-    setMounted(true);
-    const cookieToken = Cookies.get("authToken");
-    setToken(cookieToken || null);
-    setShowAuth(true); // ✅ Set to true only after mount to avoid hydration mismatch
+    const cookieToken: any = Cookies.get("authToken");
+    setToken(cookieToken);
   }, []);
-
-  if (!mounted) return null;
 
   if (!token) {
     return (
@@ -26,12 +22,10 @@ const LoginNavButton = () => {
         <CiLogin
           onClick={() => setShowAuth(true)}
           size={25}
-          style={{ cursor: "pointer" }}
+          style={{cursor:'pointer'}}
           className="ms-auto"
         />
-        {showAuth && (
-          <AuthModal show={showAuth} onClose={() => setShowAuth(false)} />
-        )}
+        <AuthModal show={showAuth} onClose={() => setShowAuth(false)} />
       </>
     );
   }
