e4df0ad4f0a1b3b6d7d1ae1774723a09b483561d
Arun pandian <arunpandian972000@gmail.com>
2025-07-09 19:14:03 +0530

    deploy v-92

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,7 +12,6 @@ import { AuthModalProvider } from "@/utils/AuthModelProvider";
 import SignupSuccessBanner from "./register/successModal";
 import ContactModal from "@/components/SubscribeModal";
 import ClientContactModalWrapper from '@/components/ClientModel'
-import Acceptcookies from '@/components/cookie-policy/cookies'
 
 async function fetchLogoData() {
   try {
@@ -184,8 +183,7 @@ export default async function RootLayout({
           enableSystem={false}
         >
           <SignupSuccessBanner />
-          <AuthModalProvider>
-  {children}
+          <AuthModalProvider>{children}
             <ClientContactModalWrapper />
           </AuthModalProvider>
         </ThemeProvider>
